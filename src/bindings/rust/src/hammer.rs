9d3d71e7e2a00770aba309d90100f3721da00939
Rick Richardson <rick.richardson@gmail.com>
2014-08-03 17:53:32 -0400

    Got the core lib working, next to convert it to a lib and add tests and docs

---
+++ src/bindings/rust/src/hammer.rs
@@ -0,0 +1,268 @@
+
+extern crate libc;
+use std::num::{ from_uint };
+use std::ptr;
+use std::mem;
+use std::io::stdio;
+
+use hammerll::*;
+
+pub struct Parser {
+  pub inner: *mut HParser
+}
+
+pub fn wrapParser(p: *mut HParser) -> Parser {
+  Parser { inner: p }
+}
+
+pub struct ParseResult {
+  pub inner: *mut HParseResult
+}
+
+impl ParseResult {
+  pub fn pprint(&self) {
+    unsafe {
+      h_pprint(libc::fdopen(libc::STDOUT_FILENO, "w".to_c_str().as_ptr()), (*self.inner).ast, 0, 0)
+    }
+  }
+}
+
+pub struct Predicate {
+  inner: HPredicate
+}
+
+pub struct Action {
+  inner: HAction
+}
+
+pub fn parse(parser: Parser, input: &str) -> Option<ParseResult> {
+  unsafe {
+    let result = h_parse(&*parser.inner, input.as_ptr(), input.char_len() as u64);
+
+    if (result == ptr::null::<HParseResult>() as *mut HParseResult) {
+      None
+    }
+    else {
+      Some(ParseResult { inner: result })
+    }
+  }
+}
+
+
+pub fn token(instr: &str) -> Parser {
+  unsafe {
+    wrapParser(h_token(instr.as_ptr(),  instr.char_len() as u64))
+  }
+}
+pub fn ch(c: u8) -> Parser {
+  unsafe {
+    wrapParser(h_ch(c))
+  }
+}
+pub fn ch_range(lower: u8, upper: u8) -> Parser {
+  unsafe {
+    wrapParser(h_ch_range(lower, upper))
+  }
+}
+pub fn int_range(p: Parser, lower: i64, upper: i64) -> Parser {
+  unsafe {
+    wrapParser(h_int_range(&*p.inner, lower, upper))
+  }
+}
+pub fn bits(len: u64, sign: i32) -> Parser {
+  unsafe {
+    wrapParser(h_bits(len, sign ))
+  }
+}
+pub fn int64() -> Parser {
+  unsafe {
+    wrapParser(h_int64())
+  }
+}
+pub fn int32() -> Parser {
+  unsafe {
+    wrapParser(h_int32())
+  }
+}
+pub fn int16() ->  Parser {
+  unsafe {
+    wrapParser(h_int16())
+  }
+}
+pub fn int8() ->  Parser {
+  unsafe {
+    wrapParser(h_int8())
+  }
+}
+pub fn uint64() ->  Parser {
+  unsafe {
+    wrapParser(h_uint64())
+  }
+}
+pub fn uint32() -> Parser {
+  unsafe {
+    wrapParser(h_uint32())
+  }
+}
+pub fn uint16() -> Parser {
+  unsafe {
+    wrapParser(h_uint16())
+  }
+}
+pub fn uint8() -> Parser {
+  unsafe {
+    wrapParser(h_uint8())
+  }
+}
+pub fn whitespace(p: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_whitespace(&*p.inner))
+  }
+}
+pub fn left(p: Parser, q: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_left(&*p.inner, &*q.inner))
+  }
+}
+pub fn right(p: Parser, q: Parser) -> Parser {
+  unsafe {
+    wrapParser(h_right(&*p.inner, &*q.inner))
+  }
+}
+pub fn middle(p: Parser, x: Parser, q: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_middle(&*p.inner, &*x.inner, &*q.inner))
+  }
+}
+pub fn action(p: Parser, a: Action, user_data: *mut ::libc::c_void) ->  Parser {
+  unsafe {
+    wrapParser(h_action(&*p.inner, a.inner, user_data))
+  }
+}
+pub fn inside(charset: &str) ->  Parser {
+  unsafe {
+    wrapParser(h_in(charset.as_ptr(), charset.char_len() as u64))
+  }
+}
+pub fn not_in(charset: &str) ->  Parser {
+  unsafe {
+    wrapParser(h_not_in(charset.as_ptr(), charset.char_len() as u64))
+  }
+}
+pub fn end() ->  Parser {
+  unsafe {
+    wrapParser(h_end_p())
+  }
+}
+pub fn nothing() ->  Parser {
+  unsafe {
+    wrapParser(h_nothing_p())
+  }
+}
+pub fn sequence(parsers: &[Parser]) -> Parser {
+  unsafe {
+    let mut ps : Vec<*mut HParser> = Vec::from_fn(parsers.len(), |i| parsers[i].inner);
+    ps.push(ptr::null::<HParser>() as *mut HParser);
+    wrapParser(h_sequence__a(ps.as_mut_ptr() as *mut *mut libc::c_void))
+  }
+}
+
+pub fn choice(parsers: &[Parser]) -> Parser {
+  unsafe {
+    let mut ps : Vec<*mut HParser> = Vec::from_fn(parsers.len(), |i| parsers[i].inner);
+    ps.push(ptr::null::<HParser>() as *mut HParser);
+    wrapParser(h_choice__a(ps.as_mut_ptr() as *mut *mut libc::c_void))
+  }
+}
+
+pub fn butnot(p1: Parser, p2: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_butnot(&*p1.inner, &*p2.inner))
+  }
+}
+pub fn difference(p1: Parser, p2: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_difference(&*p1.inner, &*p2.inner))
+  }
+}
+pub fn xor(p1: Parser, p2: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_xor(&*p1.inner, &*p2.inner))
+  }
+}
+pub fn many(p: Parser) -> Parser {
+  unsafe {
+    wrapParser(h_many(&*p.inner))
+  }
+}
+pub fn many1(p: Parser) -> Parser {
+  unsafe {
+    wrapParser(h_many1(&*p.inner))
+  }
+}
+pub fn repeat(p: Parser, n: u64) -> Parser {
+  unsafe {
+    wrapParser(h_repeat_n(&*p.inner, n))
+  }
+}
+pub fn optional(p: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_optional(&*p.inner))
+  }
+}
+pub fn ignore(p: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_ignore(&*p.inner))
+  }
+}
+pub fn sepBy(p: Parser, sep: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_sepBy(&*p.inner, &*sep.inner))
+  }
+}
+pub fn sepBy1(p: Parser, sep: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_sepBy1(&*p.inner, &*sep.inner))
+  }
+}
+pub fn epsilon() -> Parser {
+  unsafe {
+    wrapParser(h_epsilon_p())
+  }
+}
+pub fn length_value(length: Parser, value: Parser) ->  Parser {
+  unsafe {
+    wrapParser(h_length_value(&*length.inner, &*value.inner))
+  }
+}
+pub fn attr_bool(p: Parser, pred: Predicate, user_data: *mut ::libc::c_void) ->  Parser {
+  unsafe {
+    wrapParser(h_attr_bool(&*p.inner, pred.inner, user_data))
+  }
+}
+pub fn and(p: Parser) -> Parser {
+  unsafe {
+    wrapParser(h_and(&*p.inner))
+  }
+}
+pub fn not(p: Parser) -> Parser {
+  unsafe {
+    wrapParser(h_not(&*p.inner))
+  }
+}
+pub fn indirect() -> Parser {
+  unsafe {
+    wrapParser(h_indirect())
+  }
+}
+pub fn bind_indirect(indirect:Parser, inner: Parser) {
+  unsafe {
+    h_bind_indirect(indirect.inner, &*inner.inner)
+  }
+}
+pub fn with_endianness(endianness: i8, p: Parser) -> Parser {
+  unsafe {
+    wrapParser(h_with_endianness(endianness, &*p.inner))
+  }
+}
+
--- src/bindings/rust/src/main.rs
+++ src/bindings/rust/src/main.rs
@@ -1,37 +1,29 @@
+#![feature(globs)]
+
 extern crate libc;
 use std::num::{ from_uint };
 use std::ptr;
 use std::mem;
 use std::io::stdio;
-use h = hammerll;
-
-static hello : &'static str = "Hello ";
-static world : &'static str = "World";
-static there : &'static str = "there";
-static bang : &'static str = "!";
-
-fn init_parser() -> *mut h::HParser {
-  unsafe {
-    let token1 = h::h_token(hello.as_ptr(), from_uint(hello.char_len()).expect("uint conversion failed"));
-    let token2 = h::h_token(world.as_ptr(), from_uint(world.char_len()).expect("uint conversion failed"));
-    let token3 = h::h_token(there.as_ptr(), from_uint(there.char_len()).expect("uint conversion failed"));
-    let token4 = h::h_token(bang.as_ptr(), from_uint(bang.char_len()).expect("uint conversion failed"));
-    h::h_sequence(token1, h::h_choice(token2, token3, token4, ptr::null::<h::HParser>()) , ptr::null::<h::HParser>())
-  }
-}
-
-fn run_parser(parser: *mut h::HParser, input: &str) -> *mut h::HParseResult {
-  unsafe {
-    h::h_parse(&*parser, input.as_ptr(), from_uint(input.char_len()).expect("blarg"))
-  }
-}
+use h = hammer;
 
 fn main() { 
-  let parser = init_parser();
-  let result =  run_parser(parser, "Hello World!");
-  unsafe {
-    h::h_pprint(libc::fdopen(libc::STDOUT_FILENO, "w".to_c_str().as_ptr()), (*result).ast, 0, 0);
-  }
+  let parser = h::sequence([h::token("Hello, "), 
+                            h::choice([h::token("there"), 
+                                       h::token("world")]), 
+                            h::token("!")]);
+
+  println!("Try #1");
+  println!("Result: {}", h::parse(parser, "Hello, world!").map_or("fail", |r| {r.pprint(); "success!"}));
+  
+  println!("Try #2");
+  println!("Result: {}", h::parse(parser, "Hello, there!").map_or("fail", |r| {r.pprint(); "success!"}));
+  
+  println!("Try #3");
+  println!("Result: {}", h::parse(parser, "What is happening?").map_or("fail", |r| {r.pprint(); "success!"}));
 }
 
+
+mod hammer;
 mod hammerll;
+
